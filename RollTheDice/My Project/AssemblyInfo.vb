b349d46eec4ea74d96f384670f6e171ea736c04d
Smitfall <captainfalconrsmith@gmail.com>
2022-03-07 20:34:00 -0700

    Add project files.

---
+++ RollTheDice/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("RollTheDice")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("RollTheDice")>
+<Assembly: AssemblyCopyright("Copyright ©  2022")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("78c004a6-0c4c-4996-804f-eb9d4da86e5d")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ RollTheDice/RollTheDice.vb
@@ -0,0 +1,58 @@
+﻿'Fallon Smith
+'RCET0265
+'Spring 2022
+'
+
+Option Explicit On
+Option Strict On
+
+Module RollTheDice
+
+    Sub Main()
+        Dim numbers(7) As Integer
+        Dim temp As String
+        Dim seperator As String = " |"
+        Dim columnLength As Integer = 6
+        Dim lineSeperator As String = "-"
+        Dim LineLength As Integer = columnLength * (UBound(numbers) + 1)
+
+
+        For i = 1 To 1000
+            numbers(GetRandomInteger()) += 1
+        Next
+
+        For i = LBound(numbers) To UBound(numbers)
+            temp = CStr(i) & " |"
+            temp = temp.PadLeft(columnLength)
+            Console.Write(temp)
+        Next
+        Console.WriteLine()
+        'separator
+        Console.Write(StrDup(LineLength, lineSeperator))
+        'display contentents of randomNumber() array
+
+        Console.WriteLine()
+
+        For i = LBound(numbers) To UBound(numbers)
+
+            temp = CStr(numbers(i)) & seperator
+            temp = temp.PadLeft(columnLength)
+            Console.Write(temp)
+        Next
+        Console.ReadLine()
+    End Sub
+
+    Function GetRandomInteger() As Integer
+        Dim value As Integer
+        Dim temp As Single
+
+        Randomize()
+        temp = (Rnd() * 6) + 1
+        value = CInt(System.Math.Floor(CDbl(temp)))
+        System.Math.Floor(CDbl(1))
+
+        Return value
+
+    End Function
+
+End Module
